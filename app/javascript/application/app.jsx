3d13d2ca43a6bb63f26b148250506d4e9d15b7bc
Johannes Opper <xijo@pm.me>
2020-03-22 11:55:58 +0100

    Use correct decrementer

--- app/javascript/application/app.jsx
+++ app/javascript/application/app.jsx
@@ -46,10 +46,10 @@ export const App = (props) => {
     {imprintModal && <Modal onClose={toggleImprintModal}><Imprint /></Modal>}
 
     <div className='relative mb-8 mt-8 mx-auto select-none' style={{width: 240}}>
-      <img src={virus} height={35} draggable='false' className='mx-auto breathing-virus select-none' onClick={decrementCounter} onDragStart={e => e.preventDefault()} />
+      <img src={virus} height={35} draggable='false' className='mx-auto breathing-virus select-none cursor-pointer' onClick={decrementCounter} onDragStart={e => e.preventDefault()} />
     </div>
 
-    <ClickArea coords={lastClick} onClick={decrementCounter} />
+    <ClickArea coords={lastClick} onClick={decrementCounter} decrementer={props.decrementer} />
 
     <div className='text-4xl text-teal-800 text-center font-bold mb-4'>{counter}</div>
 
@@ -73,23 +73,21 @@ const ClickArea = (props) => {
     setCoords(newCoords)
   }, [props.coords])
 
-  console.log(coords)
-
   return <>
-    <Click coords={coords[0]} onClick={props.onClick} />
-    <Click coords={coords[1]} onClick={props.onClick} />
-    <Click coords={coords[2]} onClick={props.onClick} />
-    <Click coords={coords[3]} onClick={props.onClick} />
-    <Click coords={coords[4]} onClick={props.onClick} />
-    <Click coords={coords[5]} onClick={props.onClick} />
-    <Click coords={coords[6]} onClick={props.onClick} />
-    <Click coords={coords[7]} onClick={props.onClick} />
-    <Click coords={coords[8]} onClick={props.onClick} />
-    <Click coords={coords[9]} onClick={props.onClick} />
+    <Click coords={coords[0]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[1]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[2]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[3]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[4]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[5]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[6]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[7]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[8]} onClick={props.onClick} decrementer={props.decrementer} />
+    <Click coords={coords[9]} onClick={props.onClick} decrementer={props.decrementer} />
   </>
 }
 
-const Click = ({coords, onClick}) => {
+const Click = ({coords, onClick, decrementer}) => {
   if (!coords) return null
 
   const [animation, setAnimation] = useState('spaceOutRight')
@@ -99,5 +97,5 @@ const Click = ({coords, onClick}) => {
     window.setTimeout(() => setAnimation('hidden'), 200)
   }, [coords])
 
-  return <div onClick={onClick} className={`absolute text-yellow-400 font-semibold text-4xl select-none ${animation}`} style={{top: coords[1], left: coords[0]}}>+1</div>
+  return <div onClick={onClick} className={`absolute text-yellow-400 font-semibold text-4xl select-none ${animation}`} style={{top: coords[1], left: coords[0]}}>-{decrementer}</div>
 }
