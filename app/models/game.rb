87af3d1408f66fc38663878bb952f68b0caa1e02
Johannes Opper <xijo@pm.me>
2020-03-22 15:40:17 +0100

    Styles

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,8 +6,10 @@ class Game < ApplicationRecord
       amount = value.to_i
       if (amount < 2)
         d + 1
-      elsif (amount < 10)
+      elsif (amount < 5)
         d * 2
+      elsif (amount < 10)
+        d * 5
       else
         d * 10
       end
