0ae7df1608294acb758b141f53cac2edda8d51de
Johannes Opper <xijo@pm.me>
2020-03-22 01:31:00 +0100

    Remove powerup redemption, dont need it anymore

--- app/javascript/application/app.jsx
+++ app/javascript/application/app.jsx
@@ -15,10 +15,7 @@ import plop1file from './mp3s/plop1.mp3'
 import virus from './svgs/virus.svg'
 
 export const App = (props) => {
-  const powerups = JSON.parse(props.powerups)
-
   const [counter, setCounter] = useState(cookies.get('counter') || props.counter)
-  const [decrementor, setDecrementor] = useState(cookies.get('decrementor') || 1)
   const [clickAnimation, setClickAnimation] = useState(false)
   const [donateModal, toggleDonateModal] = useToggle(false)
   const [imprintModal, toggleImprintModal] = useToggle(false)
@@ -28,12 +25,7 @@ export const App = (props) => {
 
   const decrementCounter = () => {
     Math.round(Math.random()) === 1 ? plop0.play() : plop1.play()
-    setCounter(counter - decrementor)
-  }
-
-  const changeDecrementor = (newValue) => {
-    cookies.set('decrementor', newValue, {path: '/', expires: (new Date(2099, 1, 1))})
-    setDecrementor(newValue)
+    setCounter(counter - props.decrementer)
   }
 
   useEffect(() => {
@@ -51,7 +43,7 @@ export const App = (props) => {
     <div className='relative mb-8 mt-8 mx-auto' style={{width: 240}}>
       <img src={virus} height={35} className='mx-auto breathing-virus' onClick={decrementCounter} />
 
-      <div className={`absolute text-yellow-400 font-semibold text-4lg bottom-0 right-0 ${clickAnimation ? 'visible spaceOutRight' : 'hidden'}`}>-{decrementor}</div>
+      <div className={`absolute text-yellow-400 font-semibold text-4lg bottom-0 right-0 ${clickAnimation ? 'visible spaceOutRight' : 'hidden'}`}>-{props.decrementer}</div>
     </div>
 
     <div className='text-4xl text-teal-800 text-center font-bold mb-4'>{counter}</div>
@@ -60,34 +52,6 @@ export const App = (props) => {
       <button className='px-10 py-2 bg-teal-100 font-semibold rounded text-teal-800 hover:shadow-lg focus:shadow-md shadow-md cursor-pointer hover:bg-teal-200' onClick={toggleDonateModal}>BOOST</button>
     </div>
 
-    {powerups.map(powerup => <Powerup key={powerup.id} {...powerup} changeDecrementor={changeDecrementor} decrementor={decrementor} />)}
-
     <span onClick={toggleImprintModal}>Impressum</span>
-  </div>
-}
-
-const Powerup = (props) => {
-  const [redeemed, setRedeemed] = useState(cookies.get(props.id) == 'redeemed')
-
-  const redeem = () => {
-    cookies.set(props.id, 'redeemed', {path: '/', expires: (new Date(2099, 1, 1))})
-
-    if (props.amount < 2) {
-      props.changeDecrementor(props.decrementor + 1)
-    } else if (props.amount < 10) {
-      props.changeDecrementor(props.decrementor * 2)
-    } else {
-      props.changeDecrementor(props.decrementor * 10)
-    }
-    setRedeemed(true)
-  }
-  const unredeem = () => {
-    cookies.remove(props.id, {path: '/'})
-  }
-
-  if (redeemed) return null
-  return <div>{props.id} {props.amount}
-    <button onClick={redeem}>Redeem</button>
-    <button onClick={unredeem}>Unredeem</button>
   </div>
 }
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,19 @@
 class Game < ApplicationRecord
   serialize :donations, JSON
 
+  def decrementer
+    (donations || {}).inject(1) do |d, (_, value)|
+      amount = value.to_i
+      if (amount < 2)
+        d + 1
+      elsif (amount < 10)
+        d * 2
+      else
+        d * 10
+      end
+    end
+  end
+
   def powerups
     (donations || {}).map do |key, value|
       { id: Digest::MD5.hexdigest(key), amount: value.to_i }
