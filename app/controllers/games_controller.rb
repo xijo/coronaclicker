969429c84f40b0454d6b6ae3b76ab15733471841
Johannes Opper <xijo@pm.me>
2020-03-22 16:32:06 +0100

    Add comments for game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  # Create a new game, reset session and counter in cookie.
+  # Then redirect to the new game instance
   def new
     @game = Game.create(counter: GlobalNumber.last.infected)
     session.clear
@@ -6,8 +8,10 @@ class GamesController < ApplicationController
     redirect_to game_url(@game)
   end
 
+  # Show the game instance for the given game_id.
+  # Raises 404 if none was found.
   def show
-    @game = Game.find(params[:id]) #.first_or_initialize(counter: 17800)
+    @game = Game.find(params[:id])
     @global_number = GlobalNumber.last
     session[:game_id] = @game.id
   end
