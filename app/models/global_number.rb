83ae665c8ef381e95e86742fa02542f2543f21e0
Paul Ferlitz <paul.ferlitz@hotmail.com>
2020-03-23 16:19:55 +0100

    tweaked global decrementor to dono goals

--- app/models/global_number.rb
+++ app/models/global_number.rb
@@ -9,13 +9,18 @@ class GlobalNumber < ApplicationRecord
 
   def decrementer
     case received
-    when 0..99 then 0
-    when 100..199 then 1
-    when 200..499 then 2
-    when 500..999 then 3
-    when 1000..1999 then 4
-    when 2000..4999 then 5
-    else 5
+    when 0..199 then 0
+    when 200..349 then 1
+    when 350..499 then 2
+    when 500..749 then 3
+    when 750..999 then 4
+    when 1000..1399 then 5
+    when 1400..1899 then 6
+    when 1900..2499 then 7
+    when 2500..2999 then 8
+    when 3000..3999 then 9
+    when 4000..4999 then 10
+    else 11
     end
   end
 end
