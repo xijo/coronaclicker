95b6de2d9fe09645ff7ab88d29649fcb4228d7e4
Paul Ferlitz <paul.ferlitz@hotmail.com>
2020-03-25 19:02:21 +0100

    repositioned virus spots

--- app/javascript/application/virus.jsx
+++ app/javascript/application/virus.jsx
@@ -5,15 +5,17 @@ import spot_1 from './svgs/Mult_Spot_1.svg'
 import spot_2 from './svgs/Mult_Spot_2.svg'
 import padlock from './svgs/Lock.svg'
 
-export const Virus = ({virusOnClick, addifier, multiplier}) => {
+export const Virus = ({virusOnClick, spotsOnClick, addifier, multiplier}) => {
   return <div>
     <div className='relative mb-8 mx-auto select-none' style={{width: 240}}>
-      <img src={spot_1} style={{position: 'absolute', top: -20, right:-40}}/>
-      <h1 className='font-bold text-2xl' style={{position: 'absolute', top: -5, right:-5}}>-{addifier}</h1>
-      <img src={spot_2} style={{position: 'absolute', top: -75, right:-95}}/>
-      {multiplier > 0 && <h1 className='font-bold text-2xl' style={{position: 'absolute', top: -50, right:-60}}>x{multiplier}</h1>
-      || <img className='transform scale-125' src={padlock} style={{position: 'absolute', top: -50, right:-60}}/>}
-      <img src={virus} height={35} draggable='false' className='mt-20 mx-auto breathing-virus cursor-pointer' {...virusOnClick} onDragStart={e => e.preventDefault()} />
+      <div onClick={spotsOnClick} className="cursor-pointer">
+        <img src={spot_1} style={{position: 'absolute', top: -40, right:5}}/>
+        <h1 className='font-bold text-2xl text-white' style={{position: 'absolute', top: -25, right:40}}>-{addifier}</h1>
+        <img src={spot_2} style={{position: 'absolute', top: -95, right:-45}}/>
+        {multiplier > 0 && <h1 className='font-bold text-2xl text-white' style={{position: 'absolute', top: -70, right:-15}}>x{multiplier}</h1>
+        || <img className='transform scale-125' src={padlock} style={{position: 'absolute', top: -70, right:-15}}/>}
+      </div>
+      <img src={virus} height={35} draggable='false' className='mt-24 mx-auto breathing-virus cursor-pointer' {...virusOnClick} onDragStart={e => e.preventDefault()} />
     </div>
   </div>
 }
