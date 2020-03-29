4666d71a5fa29b5200c7605bb43248a13a4d2036
Paul Ferlitz <paul.ferlitz@hotmail.com>
2020-03-29 22:28:11 +0200

    fixed belohnung rounding bug

--- app/javascript/application/modals/achievementsModal.jsx
+++ app/javascript/application/modals/achievementsModal.jsx
@@ -11,7 +11,10 @@ import mystery from '../images/boosts/mystery.png'
 
 export const AchievementsModal = ({healed, toggleGoodieModal, toggleAchievements, setGoodieID}) => {
   const advRound = (number, decimal) => {
-    return Number((number).toFixed(decimal));
+    console.log(Number((number).toFixed(0)))
+    console.log(Number((number).toFixed(decimal)))
+    console.log(Number((number*100).toFixed(decimal)))
+    return Number((number*100).toFixed(decimal));
   }
 
   return <div className='m-2'>
@@ -25,12 +28,12 @@ export const AchievementsModal = ({healed, toggleGoodieModal, toggleAchievements
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 19 && <img src={haus} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(1); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 19, 2) <= 1 ? (advRound(healed / 19, 2)*100)+'%' : 'Ausgangssperre'}</span>
+        <span>{advRound(healed / 19, 2) < 100 ? advRound(healed / 19, 2)+'%' : 'Ausgangssperre'}</span>
       </a>
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 1000 && <img src={nudeln} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(2); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 1000, 2) <= 1 ? (advRound(healed / 1000, 2)*100)+'%' : 'Nudeln'}</span>
+        <span>{advRound(healed / 1000, 2) < 100 ? advRound(healed / 1000, 2)+'%' : 'Nudeln'}</span>
       </a>
     </div>
 
@@ -38,12 +41,12 @@ export const AchievementsModal = ({healed, toggleGoodieModal, toggleAchievements
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 5000 && <img src={spuckschutz} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(3); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 5000, 2) <= 1 ? (advRound(healed / 5000, 2)*100)+'%' : 'Spuckschutz'}</span>
+        <span>{advRound(healed / 5000, 2) < 100 ? advRound(healed / 5000, 2)+'%' : 'Spuckschutz'}</span>
       </a>
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 10000 && <img src={video} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(4); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 10000, 2) <= 1 ? (advRound(healed / 10000, 2)*100)+'%' : 'Videokonferenz'}</span>
+        <span>{advRound(healed / 10000, 2) < 100 ? advRound(healed / 10000, 2)+'%' : 'Videokonferenz'}</span>
       </a>
     </div>
 
@@ -51,12 +54,12 @@ export const AchievementsModal = ({healed, toggleGoodieModal, toggleAchievements
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 50000 && <img src={spritze} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(5); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 50000, 2) <= 1 ? (advRound(healed / 50000, 2)*100)+'%' : 'Impfung'}</span>
+        <span>{advRound(healed / 50000, 2) < 100 ? advRound(healed / 50000, 2)+'%' : 'Impfung'}</span>
       </a>
       <a className='btn text-center flex-1 ml-2 flex flex-col justify-evenly items-center'>
         {healed >= 100000 && <img src={handy} style={{maxHeight: 100}} onClick={() => {toggleAchievements(); setGoodieID(6); toggleGoodieModal();}}/>
         || <img src={mystery} style={{maxHeight: 100}} />}
-        <span>{advRound(healed / 100000, 2) <= 1 ? (advRound(healed / 100000, 2)*100)+'%' : 'Liebe vom Team'}</span>
+        <span>{advRound(healed / 100000, 2) < 100 ? advRound(healed / 100000, 2)+'%' : 'Liebe vom Team'}</span>
       </a>
     </div>
   </div>
